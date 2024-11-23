f93b9b53291a3d286639784427de1868e7a98750
test <test>
2024-11-22 17:43:34 -0800

    initial commit

---
+++ StringExtensions/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
