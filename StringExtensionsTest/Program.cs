46a40d43876c9ae1873409de1409386064426646
perky <78986391+perkyset@users.noreply.github.com>
2024-11-23 14:28:28 -0800

    Add files via upload

---
+++ StringExtensionsTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using StringExtensions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string test1 = "Hello";
+        string test2 = "world";
+
+        Console.WriteLine($"Test string: '{test1}' starts with uppercase: {test1.StartsWithUpper()}");
+        Console.WriteLine($"Test string: '{test2}' starts with uppercase: {test2.StartsWithUpper()}");
+
+        Console.ReadLine();
+    }
+}
